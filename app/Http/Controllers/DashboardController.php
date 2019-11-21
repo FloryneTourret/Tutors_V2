93cc32cb8210f2b686b64fb0d15a4dca1edf9a77
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2019-11-21 12:52:30 +0100

    back dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,6 +14,61 @@ class DashboardController extends Controller
 		else if(session()->get('tutor') == false)	
 			return redirect()->route('home');
 		else
-			return view('user.dashboardUser');
+		{
+			$today = date("Y-m-d H:i:s");
+			$myEventsOn = DB::table('tuteurs_events')
+			->join('tuteurs_event_participant', 'tuteurs_event_participant.id_event', '=', 'tuteurs_events.event_id')
+			->join('tuteurs_users', 'tuteurs_users.user_id', '=', 'tuteurs_event_participant.id_user')
+			->where('tuteurs_users.login', session()->get('username'))
+			->where('date_fin', '>=', $today)
+			->get();
+
+			$myEvents = DB::table('tuteurs_events')
+			->join('tuteurs_event_participant', 'tuteurs_event_participant.id_event', '=', 'tuteurs_events.event_id')
+			->join('tuteurs_users', 'tuteurs_users.user_id', '=', 'tuteurs_event_participant.id_user')
+			->where('tuteurs_users.login', session()->get('username'))
+			->get();
+
+			$mySuggestions = DB::table('tuteurs_suggestions')
+			->join('tuteurs_users', 'tuteurs_users.user_id', '=', 'tuteurs_suggestions.id_user')
+			->where('tuteurs_users.login', session()->get('username'))
+			->get();
+
+			$myHelpSessions = DB::table('tuteurs_suivi')
+			->join('tuteurs_users', function ($join) {
+				$join->on('tuteurs_suivi.id_user1', '=', 'tuteurs_users.user_id')
+				->orOn('tuteurs_suivi.id_user2', '=', 'tuteurs_users.user_id');
+			})
+			->where('tuteurs_users.login', session()->get('username'))
+			->get();
+
+			$myHelpSessionsOn = DB::table('tuteurs_suivi')
+			->join('tuteurs_users', function ($join) {
+				$join->on('tuteurs_suivi.id_user1', '=', 'tuteurs_users.user_id')
+				->orOn('tuteurs_suivi.id_user2', '=', 'tuteurs_users.user_id');
+			})
+			->where('tuteurs_users.login', session()->get('username'))
+			->where('tuteurs_suivi.resolu', 0)
+			->get();
+
+			$lastSuggestions = DB::table('tuteurs_suggestions')
+			->latest()
+			->limit(5)
+			->get();
+
+			$lastEvents = DB::table('tuteurs_events')
+			->latest()
+			->limit(5)
+			->get();
+
+			var_dump($lastEvents);
+			return view('user.dashboardUser', ['myEventsOn' => $myEventsOn,
+												'myEvents' => $myEvents,
+												'mySuggestions' => $mySuggestions,
+												'myHelpSessions' => $myHelpSessions,
+												'myHelpSessionsOn' => $myHelpSessionsOn,
+												'lastSuggestions' => $lastSuggestions,
+												'lastEvents' => $lastEvents]);
+		}
 	}
 }
