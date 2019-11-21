5496dda97d11dd043d23cf65a2ae9fdeeb8efae2
Floryne <floryne.tourret.pro@gmail.com>
2019-11-21 19:50:19 +0100

    homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,6 +21,7 @@ class HomeController extends Controller
 				->join('tuteurs_blog_categorie', 'tuteurs_blog_categorie.id_post', '=', 'tuteurs_blog.post_id')
 				->join('tuteurs_categories', 'tuteurs_categories.categorie_id', '=', 'tuteurs_blog_categorie.id_categorie')
 				->where('tuteurs_categories.nom', $category)
+				->latest()
 				->paginate(5);
 				$category = DB::table('tuteurs_categories')
 				->where('nom', $category)
@@ -32,6 +33,7 @@ class HomeController extends Controller
 			}
 			$posts = DB::table('tuteurs_blog')
 			->join('tuteurs_users', 'tuteurs_users.user_id', '=', 'tuteurs_blog.id_user')
+			->latest()
 			->paginate(5);
 
 			return view('home', compact('posts'), ['categories' => $categories]);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,58 @@
 @extends('app')
 
 @section('content')
+<div class="posts">
 	@if (isset($error))
-<h2>{{$error}}</h2>
+		<h2>{{$error}}</h2>
 	@else
-		<ul>
+	<div class="categories">
+			<a href="/">Tous les posts</a>
 			@foreach ($categories as $category)
-				<li>{{$category->nom}}</li>
+				<a href="/{{$category->nom}}">{{$category->nom}}</a>
 			@endforeach
-		</ul>
+		</div>
+
+		<div uk-grid uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 500; repeat: true">
+		@php($range = 1)
 		@foreach ($posts as $post)
-			<div>
-				<div><h2>{{$post->titre}}</h2></div>
-				<div><p>by {{$post->login}}</p></div>
-			<div><p>{{$post->resume}}</p></div>
-			</div>
-		@endforeach
-		{{$posts}}
+			@if ($range == 1)
+				<div class="uk-width-1" id="card_{{ $range++ }}">
+					<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+						<div class=" uk-cover-container">
+							<img src="{{$post->image}}" alt="" uk-cover>
+							<canvas width="600" height="400"></canvas>
+						</div>
+						<div>
+							<div class="uk-card-body">
+								<p class="uk-card-title">{{$post->titre}}</p>
+								<small>{{$post->login}}</small>
+								<p>{{$post->resume}}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			@else
+				<div class="uk-width-1-4@s">
+					<div>
+						<div class="uk-card uk-card-default">
+							<div class="uk-card-media-top">
+								<div class="img_post" style="background-image: url({{$post->image}})"></div>
+							</div>
+							<div class="uk-card-body">
+								<p class="uk-card-title">{{$post->titre}}</p>
+								<small>{{$post->login}}</small>
+							</div>
+						</div>
+					</div>
+				</div>
+			@endif
+		@endforeach	
+		</div>
+
+		<div class="pagination">
+			{{$posts}}
+		</div>
 	@endif	
+</div>
 @endsection
+			
